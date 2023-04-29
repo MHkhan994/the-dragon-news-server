cce3342d6b51bcbc71fdd85eb411098b16a8986d
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-04-29 22:33:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+var cors = require('cors')
+
+
+const catagories = require('./data/categories.json')
+const news = require('./data/news.json')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/catagories', (req, res) => {
+    res.send(catagories)
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+app.get('/catagories/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.filter(n => n.category_id === id)
+    res.send(selectedNews)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
